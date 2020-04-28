e0372f3e460ddc4ec42320c707e25e23a42bac7a
eugene-finn <1287175@gmail.com>
2020-04-29 02:34:23 +0300

    validation auth form 2 regexp

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,11 @@ import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 
+function validateEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(String(email).toLowerCase());
+}
+
 export default class Auth extends Component {
   state = {
     formControls: {
@@ -34,12 +39,41 @@ export default class Auth extends Component {
   };
 
   loginHandler = () => {};
+
   registerHandler = () => {};
+
   submitHandler = (event) => {
     event.preventDefault();
   };
+
+  validateControl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+    if (validation.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (validation.email) {
+      isValid = validateEmail(value) && isValid;
+    }
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
   onChangeHandler = (event, controlName) => {
     console.log(`${controlName}: `, event.target.value);
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+    formControls[controlName] = control;
+    this.setState({ formControls });
   };
 
   renderInputs() {
